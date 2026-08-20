d327aaa0659fbc17d9f88b40a2fbd46ba4e8f458
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-08-20 21:42:30 +0200

    fix: include project schedule options in body, not in query

--- src/api_calls/project.rs
+++ src/api_calls/project.rs
@@ -344,12 +344,11 @@ impl Ferinth<Authenticated> {
     ) -> Result<()> {
         check_id_slug(&[project_id])?;
         self.client
-            .post(
-                self.url
-                    .join_all(vec!["project", project_id, "schedule"])
-                    .with_query_json("time", time)?
-                    .with_query_json("requested_status", status)?,
-            )
+            .post(self.url.join_all(vec!["project", project_id, "schedule"]))
+            .json(&serde_json::json!({
+                "time": time,
+                "requested_status": status
+            }))
             .custom_send()
             .await?;
         Ok(())
