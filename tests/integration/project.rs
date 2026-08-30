2af2623f9ba2a631ba893c5b15689ce8b0c0f089
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-08-30 16:38:51 +0200

    tests: add project_edit_multiple test

--- tests/integration/project.rs
+++ tests/integration/project.rs
@@ -76,6 +76,38 @@ async fn get_multiple() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
+#[stubr::mock("project/edit_multiple.json")]
+async fn edit_multiple() -> anyhow::Result<()> {
+    let ctx = TestContext::new(&stubr.uri()).await?;
+
+    let body = project::EditMultipleProjectsBody {
+        categories: vec!["fabric".to_string(), "decoration".to_string()],
+        add_categories: vec![],
+        remove_categories: vec![],
+        additional_categories: vec![],
+        add_additional_categories: vec!["forge".to_string(), "adventure".to_string()],
+        remove_additional_categories: vec![],
+        donation_urls: vec![],
+        add_donation_urls: vec![],
+        remove_donation_urls: vec![project::DonationLink {
+            id: "patreon".to_string(),
+            platform: "Patreon".to_string(),
+            url: Url::parse("https://www.patreon.com/my_user").unwrap(),
+        }],
+        issues_url: None,
+        source_url: None,
+        wiki_url: Some("https://example.com".to_string()),
+        discord_url: None,
+    };
+
+    ctx.modrinth
+        .project_edit_multiple(&["AABBCCDD", "EEFFGGHH"], body)
+        .await?;
+
+    Ok(())
+}
+
+#[tokio::test]
 #[stubr::mock("project/get_random.json")]
 async fn get_random() -> anyhow::Result<()> {
     let ctx = TestContext::new(&stubr.uri()).await?;
