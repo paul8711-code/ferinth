3bdd6bb71082f162d61e3ed7fc5f4759b19425c3
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-08-30 15:39:48 +0200

    refactor: rename search functions

--- src/api_calls/project.rs
+++ src/api_calls/project.rs
@@ -11,6 +11,49 @@ use structures::{project::*, Int, UtcTime};
 
 impl<T> Ferinth<T> {
     /**
+    Search for projects using `query` string
+
+    Sort the hits by `sort`, and filter projects using the given `facets`.
+    In `facets`, only non-empty vectors will be used.
+
+    ## Example
+    ```no_run
+    # use paul8711_ferinth as ferinth;
+    # use ferinth::structures::project::{Sort, Facet};
+    # tokio_test::block_on(async {
+    # let modrinth = ferinth::Ferinth::default();
+    // When searching for 'sodium' and filtering by NeoForge mods
+    let results = modrinth.search(
+        "sodium",
+        &Sort::Downloads,
+        vec![vec![ Facet::Categories("neoforge".into()) ]],
+    ).await?;
+    // Sodium should be the result with the most downloads
+    assert_eq!(results.hits[0].slug, Some("sodium".to_owned()));
+    # Ok::<_, ferinth::Error>(()) }).unwrap()
+    ```
+    */
+    pub async fn project_search(
+        &self,
+        query: &str,
+        sort: &Sort,
+        mut facets: Vec<Vec<Facet>>,
+    ) -> Result<Response> {
+        let mut url = self
+            .url
+            .join_all(vec!["search"])
+            .with_query("query", query)
+            .with_query("index", sort);
+
+        facets.retain(|e| !e.is_empty());
+        if !facets.is_empty() {
+            url = url.with_query_json("facets", facets)?
+        }
+
+        self.client.get(url).custom_send_json().await
+    }
+
+    /**
     Search for projects using `query` string, with pagination
 
     Limit the number of responses to `limit` projects (valid 0-100), and offset the output by `offset` projects.
@@ -36,7 +79,7 @@ impl<T> Ferinth<T> {
     # Ok::<_, ferinth::Error>(()) }).unwrap()
     ```
     */
-    pub async fn search_paged(
+    pub async fn project_search_paged(
         &self,
         query: impl ToString,
         sort: Sort,
@@ -51,49 +94,6 @@ impl<T> Ferinth<T> {
             .with_query("index", sort)
             .with_query("limit", limit)
             .with_query("offset", offset);
-
-        facets.retain(|e| !e.is_empty());
-        if !facets.is_empty() {
-            url = url.with_query_json("facets", facets)?
-        }
-
-        self.client.get(url).custom_send_json().await
-    }
-
-    /**
-    Search for projects using `query` string
-
-    Sort the hits by `sort`, and filter projects using the given `facets`.
-    In `facets`, only non-empty vectors will be used.
-
-    ## Example
-    ```no_run
-    # use paul8711_ferinth as ferinth;
-    # use ferinth::structures::project::{Sort, Facet};
-    # tokio_test::block_on(async {
-    # let modrinth = ferinth::Ferinth::default();
-    // When searching for 'sodium' and filtering by NeoForge mods
-    let results = modrinth.search(
-        "sodium",
-        &Sort::Downloads,
-        vec![vec![ Facet::Categories("neoforge".into()) ]],
-    ).await?;
-    // Sodium should be the result with the most downloads
-    assert_eq!(results.hits[0].slug, Some("sodium".to_owned()));
-    # Ok::<_, ferinth::Error>(()) }).unwrap()
-    ```
-    */
-    pub async fn search(
-        &self,
-        query: &str,
-        sort: &Sort,
-        mut facets: Vec<Vec<Facet>>,
-    ) -> Result<Response> {
-        let mut url = self
-            .url
-            .join_all(vec!["search"])
-            .with_query("query", query)
-            .with_query("index", sort);
 
         facets.retain(|e| !e.is_empty());
         if !facets.is_empty() {
