74be580c7fdd99ad0d074acd686cd5e796608ab6
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-09-01 15:37:23 +0200

    tests: add version_file_delete_from_hash test

--- tests/integration/version_file.rs
+++ tests/integration/version_file.rs
@@ -10,3 +10,14 @@ async fn get_from_hash() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn file_delete_from_hash() -> anyhow::Result<()> {
+    let ctx = TestContext::new().await?;
+
+    ctx.modrinth
+        .version_file_delete_from_hash("619e250c133106bacc3e3b560839bd4b324dfda8", None)
+        .await?;
+
+    Ok(())
+}
