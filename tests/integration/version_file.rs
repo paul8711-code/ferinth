e0332a6bac7e9443516a6f0de5fe9444d985e95b
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-09-01 16:22:54 +0200

    tests: add version_get_latest_from_multiple_hashes test

--- tests/integration/version_file.rs
+++ tests/integration/version_file.rs
@@ -52,3 +52,25 @@ async fn get_from_multiple_hashes() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn get_latest_from_multiple_hashes() -> anyhow::Result<()> {
+    let ctx = TestContext::new().await?;
+
+    let filters = version::LatestVersionBody {
+        loaders: vec!["fabric".into()],
+        game_versions: vec!["1.18".into(), "1.18.1".into()],
+    };
+
+    ctx.modrinth
+        .version_get_latest_from_multiple_hashes(
+            &[
+                "619e250c133106bacc3e3b560839bd4b324dfda8",
+                "380e4a7aa7c746db8bd908991823c9f38b5569a4",
+            ],
+            &filters,
+        )
+        .await?;
+
+    Ok(())
+}
