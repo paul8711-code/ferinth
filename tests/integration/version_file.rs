b957528b53363c7f9137481838477fb94c0c78f0
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-09-01 15:48:36 +0200

    tests: add version_get_from_multiple_hashes test

--- tests/integration/version_file.rs
+++ tests/integration/version_file.rs
@@ -21,3 +21,17 @@ async fn file_delete_from_hash() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn get_from_multiple_hashes() -> anyhow::Result<()> {
+    let ctx = TestContext::new().await?;
+
+    ctx.modrinth
+        .version_get_from_multiple_hashes(&[
+            "619e250c133106bacc3e3b560839bd4b324dfda8",
+            "380e4a7aa7c746db8bd908991823c9f38b5569a4",
+        ])
+        .await?;
+
+    Ok(())
+}
