1e6bbafa598a6c00d8156d7eddc6ba64068d6e71
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-09-01 19:30:41 +0200

    tests: add unit tests for checking hashes and id slugs

--- src/api_calls/mod.rs
+++ src/api_calls/mod.rs
@@ -47,3 +47,56 @@ pub fn check_sha512_hash<S: AsRef<str>>(inputs: &[S]) -> Result<()> {
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_id_slug() {
+        // some definitely valid id/slug
+        let slug = "MMNNOOPP";
+
+        assert!(check_id_slug(&[slug]).is_ok());
+    }
+
+    #[test]
+    fn test_invalid_id_slug() {
+        // some definitely invalid id/slug
+        let slug = "M$%&\"(/§";
+
+        assert!(check_id_slug(&[slug]).is_err());
+    }
+
+    #[test]
+    fn test_valid_sha1_hash() {
+        // valid sha1 hash
+        let hash = "b789f294a2018edd24500b57829abf8a76164c6b";
+
+        assert!(check_sha1_hash(&[hash]).is_ok());
+    }
+
+    #[test]
+    fn test_invalid_sha1_hash() {
+        // invalid sha1 hash, "g" is an invalid char here
+        let hash = "e519a9f66fdfdb78d0818f3e3247f791bd9185fg";
+
+        assert!(check_sha1_hash(&[hash]).is_err());
+    }
+
+    #[test]
+    fn test_valid_sha512_hash() {
+        // valid sha512 hash
+        let hash = "5ddbb1657ccdb0422fd397fa6f355a0f59114c90cfffb80102e4219b93f863c4937e89b6c17180db256caca40329122539ca73b5eee35f5e07018a7e62f51c67";
+
+        assert!(check_sha512_hash(&[hash]).is_ok());
+    }
+
+    #[test]
+    fn test_invalid_sha512_hash() {
+        // invalid sha512 hash, has too little chars
+        let hash = "10c291b7af66feb76141f6566829cffd3bc9ff55d7e2d8c1a7d0dba4e0c6c3c9b7b061a8542bd2ab14ece7a5a85e7bef3d9eacebf11d966213259de0f2db5d7";
+
+        assert!(check_sha512_hash(&[hash]).is_err());
+    }
+}
