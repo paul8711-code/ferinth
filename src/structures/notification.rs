6cc39276163f000089f8c7ec5aadcca740371de7
Paul8711 <154304377+paul8711-code@users.noreply.github.com>
2026-09-01 18:32:36 +0200

    fix: update notification response

--- src/structures/notification.rs
+++ src/structures/notification.rs
@@ -7,6 +7,7 @@ pub struct Notification {
     pub id: ID,
     /// The ID of the user who received the notification
     pub user_id: ID,
+    pub _type: Option<NotificationType>,
     pub title: String,
     pub text: String,
     /// A _relative_ link to the related project/version
@@ -15,7 +16,15 @@ pub struct Notification {
     pub created: UtcTime,
     /// A list of actions that can be performed
     pub actions: Vec<NotificationAction>,
-    pub body: NotificationBody,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum NotificationType {
+    ProjectUpdate,
+    TeamInvite,
+    StatusChange,
+    ModeratorMessage,
 }
 
 // Undocumented struct pulled from the labrinth source code
@@ -25,45 +34,4 @@ pub struct NotificationAction {
     /// The route to call when this notification action is called.
     /// Contains the HTTP method and route respectively.
     pub action_route: (String, String),
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-#[serde(tag = "type", rename_all = "snake_case")]
-pub enum NotificationBody {
-    ProjectUpdate {
-        project_id: ID,
-        version_id: ID,
-    },
-    TeamInvite {
-        project_id: ID,
-        team_id: ID,
-        invited_by: ID,
-        role: String,
-    },
-    OrganizationInvite {
-        organization_id: ID,
-        invited_by: ID,
-        team_id: ID,
-        role: String,
-    },
-    StatusChange {
-        project_id: ID,
-        old_status: project::ProjectStatus,
-        new_status: project::ProjectStatus,
-    },
-    ModeratorMessage {
-        thread_id: ID,
-        message_id: ID,
-
-        project_id: Option<ID>,
-        report_id: Option<ID>,
-    },
-    LegacyMarkdown {
-        notification_type: Option<String>,
-        title: String,
-        text: String,
-        link: String,
-        actions: Vec<NotificationAction>,
-    },
-    Unknown,
 }
